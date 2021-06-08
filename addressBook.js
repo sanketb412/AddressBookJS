ca870ee015aedabaf67aa147f435436b0c99e5f2
sanketb412 <sanketbagde38@gmail.com>
2021-06-08 21:31:01 +0530

    Added: code for searching Contact by State abd City

--- addressBook.js
+++ addressBook.js
@@ -166,3 +166,16 @@ console.log("Counting The Number of persons in AddressBook");
 let count = contacts.reduce(count => count +=1,0);
 console.log("Number Of persons in adressBook is : " +count);
 
+function SearchCityOrState(cityOrState){
+    if(contacts.filter((p=>p.city==cityOrState)||(p=>p.state==cityOrState))){
+        console.log(contacts.toString());
+    }
+}
+
+//searching with city
+console.log("searching with city");
+SearchCityOrState('Mumbai');
+
+//searching with state
+console.log("searching with state");
+SearchCityOrState('Maharashtra');
