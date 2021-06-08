8112e3d6b4050321f46de8e5e2073879c4ed33cf
sanketb412 <sanketbagde38@gmail.com>
2021-06-08 21:15:25 +0530

    Added: code for Delete Contact

--- addressBook.js
+++ addressBook.js
@@ -145,3 +145,14 @@ if(contactindex!=-0){
 }else{
     console.log("conatct not found");
 }
+
+function deleteContactByName(firstname){
+    for(let i =0;i < contacts.length; i++){
+        if(contacts[i].firstname == firstname){
+            contacts.pop(i);
+            console.log(contacts.toString());
+        }
+    }
+}
+console.log("Contact Deleted");
+deleteContactByName('Char');
